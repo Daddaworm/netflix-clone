1fc12c22e5eb13a0d33b7e54426442067ac44b96
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-08-28 21:42:47 -0400

    added several comps fetches and css

--- src/requests.js
+++ src/requests.js
@@ -2,7 +2,7 @@
 const API_KEY = "0b7f58a80fc4c23803a33a172037e503";
 
 const requests = {
-    fetchTrending: `/trending/all/week?api_key=${API_KEY}$language=en-US`,
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}$language=en-US`,
     fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
