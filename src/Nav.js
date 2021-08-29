9b0621438d551829c7cf4b9a23ef02cf1ec2eaed
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-08-29 01:14:17 -0400

    added style function to navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import './Nav.css'
 
 const Nav = () => {
+
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                handleShow(true);
+            } else handleShow(false)
+        });
+        return () => {
+            window.removeEventListener("scroll");
+        }
+    }, [])
+
+
     return (
-        <div className="nav">
+        <div className={`nav ${show && "nav__black"}`}>
             <img
                 className="nav__logo"
                 src="https://upload.wikimedia.org/wikipedia/commons/archive/0/08/20160220053054%21Netflix_2015_logo.svg"
